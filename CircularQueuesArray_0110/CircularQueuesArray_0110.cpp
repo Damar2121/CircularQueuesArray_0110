6baa7feff6b90230ce11bddee5e64848b162c182
Damar2121 <damar.vivoy51@gmail.com>
2024-05-31 11:01:10 +0700

    membuat int main

--- CircularQueuesArray_0110/CircularQueuesArray_0110.cpp
+++ CircularQueuesArray_0110/CircularQueuesArray_0110.cpp
@@ -67,6 +67,32 @@ public:
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElements in the queue are....\n";
+
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+			}
+			cout << endl;
+		}
+		else {
+			// jika FRONT_position > REAR_position, iterasi FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+			}
+			FRONT_position = 0;
+
+			// iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+			}
+			cout << endl;
 		}
 	}
 };
+
+int main
